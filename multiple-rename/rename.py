d43f72b47deef0bfd98fc9e5f20726f0ba16e3ad
Siddhant <siddhantchhabra96@gmail.com>
2017-01-06 01:54:38 +0530

    Added Script for automated renaming of multiple files in directory from a list of names.txt

---
+++ multiple-rename/rename.py
@@ -0,0 +1,53 @@
+import os,sys
+import random
+
+names=[]
+namefile=open("correct.txt")
+for line in namefile:
+    names.append(line)
+
+def main():
+    path=raw_input("enter path to the directory \n")
+    try:
+        dirs = os.listdir(path)
+        os.chdir(path)
+    except:
+        print "error incorrect path name! enter again"
+        main()
+    else:
+        GetFiles(dirs)
+    print "Finished"
+
+def GetFiles(dirs):
+    for file in dirs:
+        index=file.rfind(".")
+        if index == -1:
+            RenameFile(file)
+            continue
+        extension=file[index:]
+        RenameFile(file,extension)
+
+def RenameFile(file,ext=""):
+    global names
+    randomName=random.choice(names)
+    newName=str(randomName[:-2])+ext
+    try:
+        os.rename(file,newName)
+    except OSError:
+        randomName=random.choice(names)
+        newName=str(randomName)+ext
+        try:
+            os.rename(file,newName)
+        except:
+            print "Unkown error occurred changing name of file:",file," to:",newName,"\n"
+    except:
+        print "Unkown error occurred changing name of file:",file," to:",newName,"\n"
+    else:
+        try:
+            i=names.index(randomName)
+        except:
+            print "\n couldn't get index of the random file name! duh what? "
+        else:
+            del names[i]
+
+main()
